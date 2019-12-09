f3cd00e59fe109c9f44c04013661e578c03638bb
mkw8263 <ruddns8213@naver.com>
2019-12-09 22:08:16 +0900

    add : test mode google auth

--- lib/page/joinMainPage.dart
+++ lib/page/joinMainPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:join/blocs/google_auth_bloc.dart';
 
 class JoinMainPage extends StatefulWidget {
   @override
@@ -12,6 +14,30 @@ class _JoinMainPageState extends State<JoinMainPage> {
         appBar: AppBar(
           title: Text("Test"),
         ),
-        body: Text("Test"));
+        body: Column(
+          children: <Widget>[
+            _builderGoogleAuth()
+          ],
+        ));
+  }
+
+  Widget _builderGoogleAuth() {
+    return StreamBuilder(
+      stream: googleAuthBloc.googleAccount,
+      builder: (BuildContext context,
+          AsyncSnapshot<GoogleSignInAccount> snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            child: RaisedButton(
+                child: const Text("Sign in"),
+                onPressed: () => googleAuthBloc.sigInGoogle()
+            ),
+          );
+        } else {
+          //Test
+          return Container(child: Text(snapshot.data.email));
+        }
+      },
+    );
   }
 }
