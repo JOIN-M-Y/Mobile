c9a895c591c184fe593db9f71d7e7a80f7ac38aa
mkw8263 <ruddns8213@naver.com>
2020-02-18 00:16:36 +0900

    add : bloc, 스터디 생성

--- lib/blocs/recommend_bloc.dart
+++ lib/blocs/recommend_bloc.dart
@@ -1,18 +1,30 @@
 import 'package:rxdart/rxdart.dart';
 
-final blocRecommend = RecommendBloc();
+final blocStudy = StudyBloc();
 
-class RecommendBloc {
-  final BehaviorSubject<List<RecommendStudy>> _subjectRecommendStudy =
+class StudyBloc {
+  final BehaviorSubject<List<RecommendStudy>> _subjectBestStudy =
       BehaviorSubject<List<RecommendStudy>>();
+
+  final BehaviorSubject<List<RecommendStudy>> _subjectRecommendStudy =
+  BehaviorSubject<List<RecommendStudy>>();
+
+  Stream<List<RecommendStudy>> get streamBestStudy =>
+      _subjectBestStudy.stream;
 
   Stream<List<RecommendStudy>> get streamRecommendStudy =>
       _subjectRecommendStudy.stream;
 
+  addAllBestStudy(List<RecommendStudy> study) =>
+      _subjectBestStudy.sink.add(study);
+
   addAllRecommendStudy(List<RecommendStudy> study) =>
       _subjectRecommendStudy.sink.add(study);
 
-  dispose() => _subjectRecommendStudy.close();
+  dispose() {
+    _subjectBestStudy.close();
+    _subjectRecommendStudy.close();
+  }
 }
 
 class RecommendStudy {
